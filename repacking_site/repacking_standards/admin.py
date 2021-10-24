a53999adfcfd3b950c1cabea386b640c2a3b32e3
Marcel Palaj <palaj.marcel@gmail.com>
2021-10-24 21:25:54 +0200

    django initialization

---
+++ repacking_site/repacking_standards/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import repacking_standard
+
+admin.site.register(repacking_standard)
---
+++ repacking_site/repacking_standards/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RepackingStandardsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'repacking_standards'
