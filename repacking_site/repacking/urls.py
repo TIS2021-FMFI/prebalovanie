5a6a29cf24f2157c6670736d5b8d32002b6f037c
Marcel Palaj <palaj.marcel@gmail.com>
2021-11-25 22:16:09 +0100

    add time counting for repacking, correct counting with pauses

--- repacking_site/repacking/urls.py
+++ repacking_site/repacking/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('repacking/history/', views.history),
     path('repacking/standards/', views.index),
     path('repacking/standards/new/', views.make_new_standard),
+    path('repacking/<str:sku_code>/repack_finished', views.finish),
+    path('repacking/<str:sku_code>/repack_paused', views.pause),
     path('repacking/<str:sku_code>/', views.detail),
-    path('repacking/<str:sku_code>/repack_finished', views.finish)
 ]
