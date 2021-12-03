4bcb746c47be8158ac8d49ac2c98f82d1099c9c3
Marcel Palaj <palaj.marcel@gmail.com>
2021-12-03 08:18:54 +0100

    move base.html to be for all apps

--- repacking_site/logs/views.py
+++ repacking_site/logs/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from logs.models import Log
 
 
-def index(request): #tu
-     log_list = Log.objects.all()
-     context = {"log_list": log_list}
-     return render(request, 'logs/index.html', context)
+def index(request):
+    log_list = Log.objects.all()
+    context = {"log_list": log_list}
+    return render(request, 'logs/index.html', context)
