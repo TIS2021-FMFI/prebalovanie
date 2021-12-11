64cdcb14dc0e84a45ffeb08b5044668df64fb472
Marcel Palaj <palaj.marcel@gmail.com>
2021-12-11 20:52:36 +0100

    add group user count

--- repacking_site/accounts/urls.py
+++ repacking_site/accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
-    path('logout/', auth_views.LogoutView.as_view(template_name='profile.html')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='index.html')),
     path('profile/', views.profile),
     path('user_list/', views.users_list),
     path('groups_list/', views.groups_list),
