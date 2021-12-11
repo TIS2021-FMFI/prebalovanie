f138969d3b9a3a0852a3d4aef89453f7370f682b
lenkadermiskova <lenka.dermiskova@gmail.com>
2021-12-11 11:57:48 +0100

    formulár label

--- repacking_site/repacking/forms.py
+++ repacking_site/repacking/forms.py
@@ -2,28 +2,28 @@ from django import forms
 
 
 class RepackingStandardForm(forms.Form):
-    SKU = forms.CharField(max_length=50, required=True)
-    COFOR = forms.CharField(max_length=50, required=True)
-    supplier = forms.CharField(max_length=50, required=True)
-    destination = forms.CharField(max_length=50, required=True)
-    items_per_move = forms.IntegerField(required=True)
-    unit_weight = forms.DecimalField(max_digits=6, decimal_places=4, required=True)
-    repacking_duration = forms.IntegerField(required=True)
-    instructions = forms.CharField(max_length=1200, required=True)
+    SKU = forms.CharField(max_length=50, required=True, label='SKU')
+    COFOR = forms.CharField(max_length=50, required=True, label='COFOR')
+    supplier = forms.CharField(max_length=50, required=True, label='Supplier')
+    destination = forms.CharField(max_length=50, required=True, label='Destination')
+    items_per_move = forms.IntegerField(required=True, label='Items per move')
+    unit_weight = forms.DecimalField(max_digits=6, decimal_places=4, required=True, label='Unit weight')
+    repacking_duration = forms.IntegerField(required=True, label='Repacking duration')
+    instructions = forms.CharField(max_length=1200, required=True, label='Instructions')
 
-    input_count_of_items_in_package = forms.IntegerField(required=True)
-    output_count_of_items_in_package = forms.IntegerField(required=True)
+    input_count_of_items_in_package = forms.IntegerField(required=True, label='Input count of items in package')
+    output_count_of_items_in_package = forms.IntegerField(required=True, label='Output count of items in package')
 
-    input_count_of_boxes_on_pallet = forms.IntegerField(required=True)
-    output_count_of_boxes_on_pallet = forms.IntegerField(required=True)
+    input_count_of_boxes_on_pallet = forms.IntegerField(required=True, label='Input count of boxes on pallet')
+    output_count_of_boxes_on_pallet = forms.IntegerField(required=True, label='Output count of boxes on pallet')
 
-    input_count_of_items_on_pallet = forms.IntegerField(required=True)
-    output_count_of_items_on_pallet = forms.IntegerField(required=True)
+    input_count_of_items_on_pallet = forms.IntegerField(required=True, label='Input count of items on pallet')
+    output_count_of_items_on_pallet = forms.IntegerField(required=True, label='Output count of items on pallet')
 
-    input_type_of_package = forms.CharField(max_length=50, required=True)
-    output_type_of_package = forms.CharField(max_length=50, required=True)
+    input_type_of_package = forms.CharField(max_length=50, required=True, label='Input type of package')
+    output_type_of_package = forms.CharField(max_length=50, required=True, label='Output type of package')
 
-    input_photos = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
-    output_photos = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    input_photos = forms.ImageField(label='Input photos', required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    output_photos = forms.ImageField(label='Output photos', required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
-    tools = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    tools = forms.ImageField(label='Tools', required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
