e0a6a0f43618863f79e71bb9401af69b349da7e2
Marcel Palaj <palaj.marcel@gmail.com>
2021-12-23 15:17:46 +0100

    try send mails.. not working ((535, b'Incorrect authentication data'))

--- repacking_site/mails/views.py
+++ repacking_site/mails/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 
 def index(request):
-    return None
+    send_mail(
+        'Pokus',
+        'Text mailu',
+        'prebalovanie@gefcoslovakia.sk',
+        ['palaj.marcel@gmail.com'],
+        fail_silently=False,
+    )
+    return render(request, 'mails/index.html')
