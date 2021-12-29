b89af2bf5553384daaba56d35fa72368fbd2ad10
Marcel Palaj <palaj.marcel@gmail.com>
2021-12-29 13:16:06 +0100

    add mail attachment

--- repacking_site/mails/views.py
+++ repacking_site/mails/views.py
@@ -1,13 +1,39 @@
+import csv
+from io import StringIO
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
+
+from repacking.models import RepackHistory
 
 
 def index(request):
-    send_mail(
+    email = EmailMessage(
         'Pokus',
         'Text mailu',
         'prebalovanie@gefcoslovakia.sk',
         ['palaj.marcel@gmail.com'],
-        fail_silently=False,
     )
+    csvfile = StringIO()
+    writer = csv.writer(csvfile, dialect='excel', delimiter=',')
+    writer.writerow(['Čas začiatku prebalu', 'Čas konca prebalu', 'Čas prebalu', 'operátori',
+                     'SKU', 'COFOR', 'Destinácia', 'ks IN', 'ks OUT',
+                     'ks v obale IN', 'ks v obale OUT', 'boxy IN', 'boxy OUT',
+                     'Trvanie prebalu podľa štandardu', 'kg/ks', 'vytvoril', 'Čas vytvorenia', 'Poznámka'])
+
+    for repack in RepackHistory.objects.all():
+        writer.writerow([repack.repack_start, repack.repack_finish, repack.repack_duration,
+                         ', '.join(map(str, repack.users.all())),
+                         repack.repacking_standard.SKU, repack.repacking_standard.COFOR,
+                         repack.repacking_standard.destination,
+                         repack.repacking_standard.input_count_of_items_on_pallet,
+                         repack.repacking_standard.output_count_of_items_on_pallet,
+                         repack.repacking_standard.input_count_of_items_in_package,
+                         repack.repacking_standard.output_count_of_items_in_package,
+                         repack.repacking_standard.input_count_of_boxes_on_pallet,
+                         repack.repacking_standard.output_count_of_boxes_on_pallet,
+                         repack.repacking_standard.repacking_duration,
+                         repack.repacking_standard.unit_weight, repack.repacking_standard.creator,
+                         repack.repacking_standard.created, repack.repacking_standard.instructions])
+    email.attach('text.csv', csvfile.getvalue(), 'text/csv')
+    email.send(fail_silently=False)
     return render(request, 'mails/index.html')
