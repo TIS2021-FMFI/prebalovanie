f4b0eb65a3ceb6880296a0a27fe3177d61f1de68
Marcel Palaj <palaj.marcel@gmail.com>
2022-01-09 10:59:10 +0100

    translate to sk, small changes in profile page

--- repacking_site/accounts/forms.py
+++ repacking_site/accounts/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class ProfileForm(forms.Form):
-    first_name = forms.CharField(max_length=50, required=True)
-    last_name = forms.CharField(max_length=50, required=True)
+    first_name = forms.CharField(max_length=50, required=True, label="Krstné meno")
+    last_name = forms.CharField(max_length=50, required=True, label="Priezvisko")
