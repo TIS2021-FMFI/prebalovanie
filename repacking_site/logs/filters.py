ebdd13d6a3aeb3a08bd24110cb944476455a08ed
matej141 <79569680+matej141@users.noreply.github.com>
2022-01-18 20:56:17 +0100

    add date picker to filter in logs page

--- repacking_site/logs/filters.py
+++ repacking_site/logs/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.forms import DateInput
 from django_filters import DateFilter
 from django_filters import CharFilter
 
@@ -6,8 +7,8 @@ from .models import *
 
 
 class LogFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='action_time', lookup_expr='gte', label="Vytvorené od")
-    end_date = DateFilter(field_name='action_time', lookup_expr='lte', label="Vytvorené do")
+    start_date = DateFilter(field_name='action_time', lookup_expr='gte', label="Vytvorené od", widget=DateInput(attrs={'type': 'date'}))
+    end_date = DateFilter(field_name='action_time', lookup_expr='lte', label="Vytvorené do", widget=DateInput(attrs={'type': 'date'}))
     text = CharFilter(field_name='text', lookup_expr='icontains', label="Informácie")
 
     class Meta:
