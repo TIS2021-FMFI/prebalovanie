7bd9a2524b82f7c2151ef1889f42a58358e5a511
Marcel Palaj <palaj.marcel@gmail.com>
2022-02-08 15:47:01 +0100

    add simple pdf export. problems w/ encoding and missing css

--- repacking_site/repacking/views.py
+++ repacking_site/repacking/views.py
@@ -1,8 +1,13 @@
 import csv
+import os
+from io import BytesIO
 
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.staticfiles import finders
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 from accounts.models import *
 from logs.models import Log
@@ -49,6 +54,72 @@ def detail(request, sku_code):
     if standard is None:
         raise Http404("Standard does not exist")
     return render(request, 'repacking/detail.html', {'standard': standard})
+
+
+@login_required
+def export(request, sku_code):
+    cancel_sessions(request)
+    standard = RepackingStandard.get_repacking_standard_by_sku(sku_code)
+    if standard is None:
+        raise Http404("Standard does not exist")
+
+    template_path = 'repacking/detail.html'
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{sku_code}.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render({'standard': standard})
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(html, dest=response, link_callback=link_callback)
+    # if error then show some funy view
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+# defining the function to convert an HTML file to a PDF file
+def html_to_pdf(template_src, context_dict=dict()):
+     template = get_template(template_src)
+     html  = template.render(context_dict)
+     result = BytesIO()
+     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+     if not pdf.err:
+         return HttpResponse(result.getvalue(), content_type='application/pdf')
+     return None
+
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
 
 
 @permission_required('accounts.history')
